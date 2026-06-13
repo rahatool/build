8544613c6fa5b47349647fac041badcc81218082
Mahdi <mahdi.nezarati@gmail.com>
2026-06-13 21:31:22 +0330

    refactor: upgrade fre to v2.8.9

--- jsx-runtime.ts
+++ jsx-runtime.ts
@@ -1,4 +1,4 @@
 // export { h as jsx, h as jsxs, Fragment } from "./fre/index.ts"
 
-let { h, Fragment } = await import("https://esm.sh/gh/frejs/fre@2.6.4/src");
+let { h, Fragment } = await import("https://esm.sh/gh/frejs/fre@2.8.9/src");
 export { h as jsx, h as jsxs, Fragment };
